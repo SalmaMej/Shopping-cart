0137cf4ed04a5ee7fd211abb08af6eb8338f6267
SalmaMej <72382328+SalmaMej@users.noreply.github.com>
2020-10-25 20:48:03 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -63,7 +63,6 @@ function updateCartTotal() {
         var quantity = quantityElement.value
         total = total + (price * quantity)
     }
-     /* total = Math.round(total * 100) / 100 */
     
     
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
